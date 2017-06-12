ad487764d673b49bb567853130b7e2da173e27ad
kirill <best.portfoleo@gmail.com>
2017-06-12 09:39:13 +0500

    Remove json-parser, use content option as content source for i18n module

--- src/I18n.ts
+++ src/I18n.ts
@@ -5,37 +5,44 @@ import IntlMessageFormat from 'intl-messageformat';
 import {
   I18nInterface,
   I18nOptionsInterface,
-  JSONParserInterface,
   Obj
 } from './types';
 
 
 class I18n implements I18nInterface {
-  private _jsonParser: JSONParserInterface;
+  private _currentLocale: string = 'en';
+  private _content: Obj<any>;
 
-  constructor(options: I18nOptionsInterface) {
-    const {
-      jsonParser,
-      directoryPath,
-      currentLocale
-    } = options;
-
-    this._jsonParser = jsonParser;
-    this._jsonParser.read(this._getPathToFile(directoryPath, currentLocale));
-  }
-
-  _getPathToFile(directoryPath:string, fileName: string): string {
-    return path.join(directoryPath, `${fileName}.json`);
-  }
-
-  _formatMessage(message: string, data?: Obj<any>) {
+  private _formatMessage(message: string, data?: Obj<any>) {
     const intlMessage = new IntlMessageFormat(message);
 
     return intlMessage.format(data);
   }
 
+  private _resolveObjPath(obj: Obj<any>, pathToValue: string): string {
+    return pathToValue.split('.').reduce((resultValue: Obj<any>, currentKey: string) => {
+      return resultValue && resultValue[currentKey];
+    }, obj) || null;
+  }
+
+  private _resolvePath(pathToValue: string): string {
+    return this._resolveObjPath(this._content, pathToValue);
+  }
+
+
+  constructor(options: I18nOptionsInterface) {
+    const {
+      content,
+      locale
+    } = options;
+
+    this._content = content;
+
+    if (locale) { this._currentLocale = locale; }
+  }
+
   t(pathToValue: string, data?: Obj<any>): string {
-    return this._formatMessage(this._jsonParser.resolvePath(pathToValue), data);
+    return this._formatMessage(this._resolvePath(pathToValue), data);
   }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -2,25 +2,18 @@ interface I18nInterface {
   t(pathToValue: string, data?: Obj<any>): string;
 }
 
-interface JSONParserInterface {
-  read(pathToFile: string): Obj<any>;
-  resolvePath(pathToValue: string): any;
-}
-
 type Obj<T> = {
   [key: string]: T;
 }
 
 type I18nOptionsInterface = {
-  jsonParser: JSONParserInterface;
-  directoryPath: string;
-  currentLocale: string;
+  content: Obj<any>;
+  locale?: string;
 }
 
 
 export {
   Obj,
   I18nInterface,
-  JSONParserInterface,
   I18nOptionsInterface
 };
